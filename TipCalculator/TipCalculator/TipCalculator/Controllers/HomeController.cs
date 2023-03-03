2a4e7861766b2cc88697ef9df29147ed531724cc
Ryan <ryan.khlee@yahoo.ca>
2023-03-02 23:30:42 -0500

    TIp calculator Model Created and home controller has been modified slightly

---
+++ TipCalculator/TipCalculator/TipCalculator/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TipCalculator.Models;
+
+namespace TipCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Fifteen = 0;
+            ViewBag.Twenty = 0;
+            ViewBag.TwentyFive = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(TipsCalculator calc)
+        {
+            if(ModelState.IsValid)
+            {
+                ViewBag.Fifteen = calc.CalculateTip(0.15);
+                ViewBag.Twenty = calc.CalculateTip(0.2);
+                ViewBag.TwentyFive = calc.CalculateTip(0.25);
+            }
+            else
+            {
+                ViewBag.Fifteen = 0;
+                ViewBag.Twenty = 0;
+                ViewBag.TwentyFive = 0;
+            }
+            return View(calc);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TipCalculator/TipCalculator/TipCalculator/Models/TipsCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TipCalculator.Models
+{
+    public class TipsCalculator
+    {
+        [Required(ErrorMessage ="Error, invalid input")]
+        [Range(0, 10000, ErrorMessage ="Error, the item cost should be in the range of 0 to 10,000")]
+        public double? ItemCost { get; set; }
+        public double CalculateTip(double percent)
+        {
+            if (ItemCost.HasValue)
+            {
+                var tip = percent * ItemCost.Value;
+            }
+            return 0.0;
+        }
+    }
+}
